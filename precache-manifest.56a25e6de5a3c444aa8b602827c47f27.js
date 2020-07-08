eaea8e1b6c53889c71a4aaeecccca4d54d33045b
akhiln95 <akhilnalliboina@gmail.com>
2020-07-08 18:00:52 +0200

    Updates

---
+++ precache-manifest.56a25e6de5a3c444aa8b602827c47f27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4404db825da7030c665550715f7fa54",
+    "url": "/Backward-and-Forward-Search/index.html"
+  },
+  {
+    "revision": "6180d3d224d8bbdb7ea3",
+    "url": "/Backward-and-Forward-Search/static/css/2.92a0e85f.chunk.css"
+  },
+  {
+    "revision": "261f036a688fa25412b7",
+    "url": "/Backward-and-Forward-Search/static/css/main.728e476a.chunk.css"
+  },
+  {
+    "revision": "6180d3d224d8bbdb7ea3",
+    "url": "/Backward-and-Forward-Search/static/js/2.eec20478.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Backward-and-Forward-Search/static/js/2.eec20478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261f036a688fa25412b7",
+    "url": "/Backward-and-Forward-Search/static/js/main.e6e28c69.chunk.js"
+  },
+  {
+    "revision": "3e2bb86d3a4f5a3c60ee",
+    "url": "/Backward-and-Forward-Search/static/js/runtime-main.d76af43d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Backward-and-Forward-Search/precache-manifest.56a25e6de5a3c444aa8b602827c47f27.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Backward-and-Forward-Search/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
